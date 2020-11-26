1b1a8c6f22badefb059c4417b4088525a4720dd5
Anand <anandsnair2005@gmail.com>
2020-11-26 23:34:43 +0530

    Initial push

---
+++ src/main/java/application/UserInterface.java
@@ -0,0 +1,60 @@
+package application;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class UserInterface {
+
+    private Scanner scanner;
+    private TodoDao database;
+
+    public UserInterface(Scanner scanner, TodoDao database) {
+        this.scanner = scanner;
+        this.database = database;
+    }
+
+    public void start() throws SQLException {
+        while (true) {
+            System.out.println("");
+            System.out.println("Enter command:");
+            System.out.println("1) list");
+            System.out.println("2) add");
+            System.out.println("3) mark as done");
+            System.out.println("4) remove");
+            System.out.println("x) quit");
+
+            System.out.print("> ");
+            String command = this.scanner.nextLine();
+            if (command.equals("x")) {
+                break;
+            }
+
+            // implement the functionality here
+
+            if (command.equals("1")) {
+                System.out.println(database.list());
+            }
+
+            if (command.equals("2")) {
+                System.out.println("Enter name");
+                String inputName = this.scanner.nextLine();
+                System.out.println("Enter description");
+                String inputDescription = this.scanner.nextLine();
+                database.add(new Todo(inputName,inputDescription,false));
+            }
+            if (command.equals("3")) {
+                System.out.println("Which todo should be marked as done (give the id)?");
+                int idToMarkAsDone = Integer.valueOf(this.scanner.nextLine());
+                database.markAsDone(idToMarkAsDone);
+            }
+            if (command.equals("4")) {
+                System.out.println("Which todo should be removed (give the id)?");
+                int idToRemove = Integer.valueOf(this.scanner.nextLine());
+                database.remove(idToRemove);
+            }
+        }
+
+        System.out.println("Thank you!");
+    }
+
+}
